2a92f5a8b04845fad7344624d7bd883878f50e2f
himaloyash <himaloyashis98@gmail.com>
2023-10-27 01:03:05 +0600

    hero icon

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
+
+
     return (
-        <div className=' no-underline bg-purple-700'>
-            <Link className='no-underline bg-purple-800'>Romi Finance</Link>
-            <Link className='no-underline'>Home</Link>
-            <Link className='no-underline'>Home</Link>
-            <Link className='no-underline'>Home</Link>
-            <Link className='no-underline'>Home</Link>
-            <Link className='no-underline'>Home</Link>
+        <div className=' no-underline bg-purple-700 gap-3'>
+            <div>
+
+            </div>
+            <div>
+                <Link className='no-underline '>Romi Finance</Link>
+                <Link className='no-underline ml-4'>Home</Link>
+                <Link className='no-underline ml-4'>Home</Link>
+                <Link className='no-underline ml-4'>Home</Link>
+                <Link className='no-underline ml-4'>Home</Link>
+                <Link className='no-underline ml-4'>Home</Link>
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css';
 import { RouterProvider } from 'react-router-dom'
 import router from './pages/Routes/Routes'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router}>
-
   </RouterProvider>
 )
