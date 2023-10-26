848d9d0f5063d8ffc83d41ac97bd64c07eb1f853
himaloyash <himaloyashis98@gmail.com>
2023-10-26 23:52:42 +0600

    Link with nav

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div>
-            
+            <Link>Romi Finance</Link>
+            <Link>Home</Link>
+            <Link>Home</Link>
+            <Link>Home</Link>
+            <Link>Home</Link>
+            <Link>Home</Link>
         </div>
     );
 };
