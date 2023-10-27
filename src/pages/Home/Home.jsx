61525336f5c49c388ef780a08e93a5c4935004cf
himaloyash <himaloyashis98@gmail.com>
2023-10-28 00:27:41 +0600

    exact color and pading

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import { ArrowTrendingUpIcon, ArrowTopRightOnSquareIcon, UserGroupIcon } from '@
 
 const Home = () => {
     return (
-        <div className='bg-[#0F0F0F] shadow-inner p-6 '>
+        <div className='bg-[#0F0F0F] shadow-inner p-6 pb-24'>
             <div className=' w-3/6 font-bold'>
                 <p className='text-6xl text-white'>Decentralized Perpetual Exchange</p>
                 <p className='text-xl mt-2 w-2/3 text-slate-300'>Trade BTC, ETH, AVAX and other top cryptocurrencies with up to 30x leverage directly from your wallet</p>
@@ -17,7 +17,7 @@ const Home = () => {
 
 
                     <div>
-                        <p className='text-3xl'>okok</p>
+                        <p className='text-3xl'>Total Trending volume</p>
                         <p className='text-3xl'>$0</p>
                     </div>
                 </div>
@@ -26,7 +26,7 @@ const Home = () => {
                     <ArrowTopRightOnSquareIcon className="h-14 w-16 hover:bg-purple-700 p-1 rounded-md text-blue-500 cursor-pointer" />
 
                     <div>
-                        <p className='text-3xl'>okok</p>
+                        <p className='text-3xl'>Open interest</p>
                         <p className='text-3xl'>$0</p>
                     </div>
                 </div>
@@ -34,7 +34,7 @@ const Home = () => {
 
                     <UserGroupIcon className="h-14 w-16 hover:bg-purple-700 p-1 rounded-md text-blue-500 cursor-pointer" />
                     <div>
-                        <p className='text-3xl'>okok</p>
+                        <p className='text-3xl'>Total User</p>
                         <p className='text-3xl'>$0</p>
                     </div>
                 </div>
