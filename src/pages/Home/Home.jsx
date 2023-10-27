e966958c404877a4c9bace24abbcd7458aa8a7f4
himaloyash <himaloyashis98@gmail.com>
2023-10-27 16:53:40 +0600

    ok ay

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='bg-[#0F0F0F] shadow-inner p-6 text-white'>
-            <p className=''>All about Home</p>
-            
+        <div className='bg-[#0F0F0F] shadow-inner p-6 '>
+            <div className=' w-3/6 font-bold'>
+                <p className='text-6xl text-white'>Decentralized Perpetual Exchange</p>
+                <p className='text-xl mt-2 w-2/3 text-slate-300'>Trade BTC, ETH, AVAX and other top cryptocurrencies with up to 30x leverage directly from your wallet</p>
+                <br />
+                <button className='hover:bg-[#266b76] duration-500 bg-[#2E97A7] py-2 rounded-md px-3'>Launch Exchange</button>
+            </div>
+
         </div>
     );
 };
