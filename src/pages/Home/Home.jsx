d0819eaf5ca1763bc9170dd1a785ea98bd4ab642
himaloyash <himaloyashis98@gmail.com>
2023-10-27 17:20:34 +0600

    box shadow not setted

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,6 +9,29 @@ const Home = () => {
                 <br />
                 <button className='hover:bg-[#266b76] duration-500 bg-[#2E97A7] py-2 rounded-md px-3'>Launch Exchange</button>
             </div>
+            <div className='text-white gap-3  grid md:grid-cols-3 lg:grid-cols-3 mt-3'>
+                <div className='w-3/3 border border-gray-200 rounded-md shadow-md flex gap-8 items-center px-4 py-5'>
+                    <p>oklk</p>
+                    <div>
+                        <p >okok</p>
+                        <p >$0</p>
+                    </div>
+                </div>
+                <div className='w-3/3 border border-gray-200 rounded-md shadow-md flex gap-8 items-center px-4 py-5'>
+                    <p>oklk</p>
+                    <div>
+                        <p >okok</p>
+                        <p >$0</p>
+                    </div>
+                </div>
+                <div className='w-3/3 border border-gray-200 rounded-md shadow-md flex gap-8 items-center px-4 py-5'>
+                    <p>oklk</p>
+                    <div>
+                        <p >okok</p>
+                        <p >$0</p>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
