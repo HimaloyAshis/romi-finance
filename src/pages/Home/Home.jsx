a4ca900cf4d5fcca2c77a8996eacde9cf1ada049
himaloyash <himaloyashis98@gmail.com>
2023-10-27 16:35:24 +0600

    home text

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div className='bg-[#0F0F0F] shadow-inner p-6 text-white'>
             <p className=''>All about Home</p>
+            
         </div>
     );
 };
