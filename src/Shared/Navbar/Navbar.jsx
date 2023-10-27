cff843dcc45a0a03b363a9c8f4eb6c581b6340b6
himaloyash <himaloyashis98@gmail.com>
2023-10-27 14:01:18 +0600

    nav done

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -8,23 +8,23 @@ const Navbar = () => {
 
 
     return (
-        <nav className='bg-purple-400 py-2 px-2'>
+        <nav className='bg-purple-400 py-2'>
             <div onClick={() => setOpen(!open)} className='md:hidden'>
                 <span>
                     {
-                        open === false ? <Bars3Icon className="h-6 w-6 text-blue-500 cursor-pointer" />
+                        open === false ? <Bars3Icon className="h-6 w-6 hover:bg-purple-700 text-blue-500 cursor-pointer" />
                             :
-                            <XMarkIcon className="h-6 w-6 text-blue-500 cursor-pointer" />
+                            <XMarkIcon className="h-6 w-6 hover:bg-purple-700 text-blue-500 cursor-pointer" />
                     }
                 </span>
 
             </div>
-            <ul className={` absolute md:static bg-purple-400 duration-500 md:flex p-3  ${open ? "top-6" : "-top-48"}`}>
-                <li className=''><Link >Romi Finance</Link></li>
-                <li> <Link >Home</Link></li>
-                <li> <Link >About</Link></li>
-                <li> <Link >Dashboard</Link></li>
-                <li> <Link >Earn</Link></li>
+            <ul className={` gap-4 items-center justify-center absolute md:static bg-purple-400 duration-500 md:flex p-3 mt-2 ml-0  ${open ? "top-6" : "-top-48"}`}>
+                <li className='hover:bg-purple-700 rounded-md p-1'><Link >Romi Finance</Link></li>
+                <li className='hover:bg-purple-700 rounded-md p-1'> <Link >Home</Link></li>
+                <li className='hover:bg-purple-700 rounded-md p-1'> <Link >About</Link></li>
+                <li className='hover:bg-purple-700 rounded-md p-1'> <Link >Dashboard</Link></li>
+                <li className='hover:bg-purple-700 rounded-md p-1'> <Link >Earn</Link></li>
             </ul>
 
         </nav>
